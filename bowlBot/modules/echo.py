abed2c1a2d7dd555c4145963f21b0e82363735a3
_ <@>
2023-05-10 11:55:27 +0000

    Added docstrings, minor changes

--- bowlBot/modules/echo.py
+++ bowlBot/modules/echo.py
@@ -2,15 +2,44 @@ from ModuleBase import ModuleBase
 from discord.ext import commands
 
 class Echo(ModuleBase):
+    """
+    A module implementing the `echo` command for testing purposes.
+
+    Attributes:
+        bot: Bot this module is attached to.
+    """
+
     def __init__(self, bot):
         self.bot = bot
 
 
     @commands.command()
-    async def echo(self, ctx):
-        reply_text = ctx.message.content.removeprefix("!echo").strip()
-        print("echo:", reply_text)
-        await ctx.send(reply_text)
+    async def echo(self, ctx, text: str):
+        """
+        Sends a message with the content of its argument.
+
+        Args:
+            ctx (): Command context,
+            text: the string to be echoed back.
+        """
+        await ctx.send(text)
+
+
+    @staticmethod
+    def getInformation() -> dict:
+        """
+        Some basic information for the module.
+
+        Returns:
+            A dict with information on this module.
+        """
+
+        return {
+            "author": "md",
+            "shortDesc": "The `echo` command.",
+            "description": "A test module that implements the `echo` command.",
+            "commands": "echo  : sends back the argument string unchanged."
+        }
 
 
 async def setup(bot):
